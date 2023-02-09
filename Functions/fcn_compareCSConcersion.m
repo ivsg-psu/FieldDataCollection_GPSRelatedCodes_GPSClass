3c713ff203a3ffef03cdcff6228fe462812eba73
bwspsu <wxb41@psu.edu>
2023-02-09 14:49:55 -0500

    Create fcn_compareCSConcersion.m

---
+++ Functions/fcn_compareCSConcersion.m
@@ -0,0 +1,14 @@
+function fcn_compareCSConcersion()
+% This function compares the coordinate system conversion algorithms:
+% Algorithm 1: From GPS class
+% Algorithm 2: From Satya's conversion code
+% Algorithm 3: From MATLAB built-in code
+
+
+
+
+
+
+
+
+end
